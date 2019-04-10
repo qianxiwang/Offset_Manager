8697288c4c405a2539af69707d59f1ceeff4efe9
mac <2734756709@qq.com>
2019-04-10 18:41:43 +0800

    对接kafka的Direct方式

--- src/main/scala/com/interview/OffsetManagerApp.scala
+++ src/main/scala/com/interview/OffsetManagerApp.scala
@@ -1,16 +1,37 @@
 package com.interview
 
+import kafka.serializer.StringDecoder
 import org.apache.spark.SparkConf
+import org.apache.spark.internal.Logging
+import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
-class OffsetManagerApp {
+
+/**
+  * offset没管理：ZK/Hbase/Redis
+  */
+class OffsetManagerApp extends Logging {
 
   def main(args: Array[String]): Unit = {
 
     val conf = new SparkConf().setMaster("local[2]").setAppName("OffsetManagerApp")
     val ssc = new StreamingContext(conf, Seconds(10))
 
+    val kafkaParams = Map[String, String](
+      "metadata.broker.list" -> "macbookpro:9092",
+      "auto.offset.reset" -> "smallest",
+      "group.id" -> "pkgroup"
+    )
 
+    val topics = "pkoffset".split(",").toSet
+
+    val stream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topics)
+
+
+    stream.foreachRDD(rdd => {
+
+      logWarning("pk哥统计结果：" + rdd.count())
+    })
 
     ssc.start()
     ssc.awaitTermination()
